1a0c5a0341c41c557d517467ee25cf5ec4fb7d7a
Cybergen <41336298+Cybergen300@users.noreply.github.com>
2020-12-02 15:14:43 +0100

    Update interaction.js

--- src/store/interaction.js
+++ src/store/interaction.js
@@ -69,12 +69,6 @@ import synthsOil from '../abis/sOil.json'
 import Exchange from '../abis/Exchange.json'
 import { ETHER_ADDRESS } from '../helpers'
 
-export const loadWeb3 = (dispatch) => {
-  const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
-  dispatch(web3Loaded(web3))
-  return web3
-}
-
 export const loadWeb3 = async() => {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
