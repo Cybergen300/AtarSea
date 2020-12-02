9e33bebf287829b7426716485aeb02c35ddc5738
Cybergen <41336298+Cybergen300@users.noreply.github.com>
2020-12-02 15:14:10 +0100

    Update interaction.js

--- src/store/interaction.js
+++ src/store/interaction.js
@@ -75,6 +75,19 @@ export const loadWeb3 = (dispatch) => {
   return web3
 }
 
+export const loadWeb3 = async() => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('Non_ethereum browser detected, you should consider trying Metamask')
+    }
+}
+
 export const loadAccount = async (web3, dispatch) => {
   const accounts = await web3.eth.getAccounts()
   const account = accounts[0]
